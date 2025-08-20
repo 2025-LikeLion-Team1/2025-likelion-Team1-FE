b73c4573431af370f3b992df2d95f1c9f13c24eb
jihyun1123 <janet9182@naver.com>
2025-08-20 19:34:44 +0900

    feat: QuestionProcessThree 인디케이터 구현

---
+++ QuestionThree/src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./tailwind.css";
+import { Three } from "./Three/Three";
+
+export default function App() {
+  return (
+    <div>
+      <Three />
+    </div>
+  );
+}
