8854fd1958901cde9de5ae5178effe658cfbb2ec
jihyun1123 <janet9182@naver.com>
2025-08-20 21:11:01 +0900

    feat : 각 버튼들 구현 및 커스텀 스크롤바 구현

---
+++ AnswerBoard/src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./tailwind.css";
+import { AnswerBoard } from "./AnswerBoard/AnswerBoard";
+
+export default function App() {
+  return (
+    <div>
+      <AnswerBoard />
+    </div>
+  );
+}
